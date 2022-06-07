9716d6927708fbcf67d6870ba241a0055279a3f1
Chuck Konkol <ckonkol@gmail.com>
2022-06-07 12:44:17 -0700

    Update share.js

--- share.js
+++ share.js
@@ -64,12 +64,7 @@ var section = "";
                });
  var website = "https://www.ckonkol.com/aquareference/myaqua.php?id=" + geturl
  document.getElementById("MyList").innerHTML = '';
- //document.getElementById("MyList").innerHTML = '<iframe frameborder="0" src=' + website + ' width="100%" height="12000"></iframe>';  
- var showweb = '<body style="margin:0px;padding:0px;overflow:hidden">
-    <iframe src=' + website + ' frameborder="0" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px" height="100%" width="100%"></iframe>
-</body>
-';
-  document.getElementById("MyList").innerHTML = showweb;  
+ document.getElementById("MyList").innerHTML = '<iframe frameborder="0" src=' + website + ' width="100%" height="1200"></iframe>';  
 }
 
 function closeWindow() {
