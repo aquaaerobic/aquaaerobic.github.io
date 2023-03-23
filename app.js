e4789fb8008be3c317732ad9a9460bd7ade69733
Chuck Konkol - AquaDevelopers <myaquaapps@gmail.com>
2023-03-22 21:09:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,14 +11,11 @@
   firebase.initializeApp(firebaseConfig);
   var db = firebase.firestore();
   
-$(document).ready(function() {
-  
-//$('html').append('<center><img src="home.jpg" width="400"></center>');
-  
-$('body').css('display', 'none')
-.delay(1000).fadeIn(500, function() {
-  $('.onLoad').fadeOut(500);
-});
+$(document).ready(function() { 
+//$('body').css('display', 'none')
+//.delay(1000).fadeIn(500, function() {
+//  $('.onLoad').fadeOut(500);
+//});
   
 var phonename = "Android";
 var d = new Date();
