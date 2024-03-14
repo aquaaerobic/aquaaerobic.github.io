40d3af346399f7e501371772e5f47473c916593b
AquaDevelopers - Chuck Konkol <myaquaapps@gmail.com>
2024-03-13 21:41:07 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -39,6 +39,18 @@ jQuery.get('notification.txt', function(data) {
 });
 }
 
+function  callback() {
+jQuery.get('notification.txt', function(data) {
+         //alert(data.length);
+          // initialize title and body variables
+        if (data.length > 3){
+        var notifs = data;
+        var Titles = "1-Minute Guide";
+    createDialog(notifs , Titles);
+}
+});
+}
+
 var createDialog = function(text , title) {
     //create dialog <div> shell
     var dialog =  "<div id=dialog <h1>" + text + "</h1></div>";
