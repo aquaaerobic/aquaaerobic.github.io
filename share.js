4be0249e82af0dd960f33b9f24c04de188bd88dc
AquaDevelopers - Chuck Konkol <myaquaapps@gmail.com>
2026-01-22 22:34:34 -0600

    Update visited field to include menu in share.js

--- share.js
+++ share.js
@@ -51,7 +51,7 @@ var section = "";
     db.collection("users").add({
     date: myTimestamp,
     device: devicetype,
-    visited: name
+    visited: menu + '/' + name
 })
 .then(function(docRef) {
     console.log("Document written with ID: ", docRef.id);
